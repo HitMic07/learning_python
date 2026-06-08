03bb7ef65b798afc40f377fdf989d23a7dd6cf87
HitMic <omkarkalyana2072@gmail.com>
2026-06-08 10:59:29 +0530

    Add Python files

---
+++ classes.py
@@ -0,0 +1,13 @@
+# IN CLASSES THE FIRST LETTER IS ALWAYS CAPITAL AND THE MORE THAN 2 WORDS ARE SEPERATED BY CAPITAL LETTER
+
+class Point:
+    def move(self):
+        print("move")
+
+    def draw(self):
+        print("draw")
+
+# CLASSES ARE JUST BLUEPRINT FOR CREATING OBJECT
+
+point1 = Point()
+point1.draw()
